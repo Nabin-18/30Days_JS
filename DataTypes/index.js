527533479cbe94e4113f05ad45acc1e19cf912de
Nabin khanal <khanalnabin310@gmail.com>
2024-07-13 11:39:45 +0545

    first commit

---
+++ DataTypes/index.js
@@ -0,0 +1,49 @@
+//Variable decleration 
+//using var 
+
+var myNumber=10;
+console.log(myNumber)
+
+let name="Nabin Khanal";
+console.log(name);
+
+
+//constant declaration 
+
+const boolValue=true;
+console.log(boolValue)
+
+//Data types in JavaScript
+
+const num =10;
+console.log(typeof num);
+
+const str="Hello";
+console.log(typeof str);
+
+const bool=true;
+console.log(typeof bool);
+
+const obj={};
+console.log(typeof obj);
+
+const arr=[];
+console.log(typeof arr);
+//Array is also object in JavaScript
+
+
+//Reassigning the value of variable
+
+let a=5;
+console.log(a);
+a=10;
+console.log(a);
+
+//now using const
+
+const b=5;
+console.log(b);
+// b=10; this will give error
+// console.log(b);
+
+
