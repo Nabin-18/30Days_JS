ee17144e490f3e682bca08ff8bb51ce728f4c978
Nabin khanal <khanalnabin310@gmail.com>
2024-08-20 22:44:44 +0545

    function

--- Function/index.js
+++ Function/index.js
@@ -71,4 +71,12 @@ const beautiful=(username)=>{
 beautiful("hitesth sir")
 
 
+// Example usage
+let number1 = 5;
+let number2 = 3;
+let sum = addNumbers(number1, number2);
+
+console.log(sum); // Output: 8
+
+
 
